893cf4af6f15d14138c22b062fd8b285c7913d07
Wang, Ruiyao <wangchf@163.com>
2022-07-05 17:31:35 -0700

    Never overwrite existing rules.

--- background.js
+++ background.js
@@ -1,8 +1,15 @@
 let shortUrlRules = [
   // This is based on a golink rule: https://calendar.google.com/calendar/u/0/r/week/{*}/{*}/{*}
-  ["go/week/$1/$2/$3", "calendar.google.com/calendar/u/0/r/week/{*}/{*}/{*}"], 
+  ["go/week/$1/$2/$3", "calendar.google.com/calendar/u/0/r/week/{*}/{*}/{*}"],
 ];
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ shortUrlRules });
+  chrome.storage.sync.get('shortUrlRules', function (rules) {
+    if (typeof rules === 'undefined') {
+      // if not set then set it 
+      chrome.storage.sync.set({ shortUrlRules });
+    } else {
+      // if already set it then nothing to do 
+    }
+  });
 });
